696a958f21c6a5f64d8a6af50a1d35f5718bff61
xingfengwxx <1099420259@qq.com>
2019-07-02 09:44:41 +0800

    Initial commit

---
+++ Day01/hello.py
@@ -0,0 +1,10 @@
+"""
+1.单行注释 - 以#和空格开头的部分
+2.多行注释 - 三个引号开头，三个引号结尾
+"""
+
+print('hello, world!')
+# print("你好，世界")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
